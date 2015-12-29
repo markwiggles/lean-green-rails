10527b16c4b564e870a2a248d7d7a8325ec95a5b
Mark Wigglesworth <markwigg@live.com>
2015-12-29 20:14:42 +1000

    added custom modal dialogs

---
+++ frontend/app/components/app-modal.js
@@ -0,0 +1,21 @@
+import ModalDialog from 'ember-modal-dialog/components/modal-dialog';
+import modalLayout from '../templates/components/app-modal'
+
+export default ModalDialog.extend({
+	containerClassNames: "app-modal",
+	targetAttachment: "none",
+	layout: modalLayout,
+	isShowingBody: true, // allow the modal to be created (offscreen)
+	actions: {
+		toggleBody() { //toggles whether the modal is seen
+			this.toggleProperty('isShowingBody');
+		}
+	},
+	didInsertElement: function() {
+		setTimeout(function() { // after a time, animate the modal onto screen
+			$('.app-modal').css({
+				'right': '2%'
+			});
+		}, 2000)
+	}
+});
--- frontend/app/routes/index.js
+++ frontend/app/routes/index.js
@@ -3,19 +3,4 @@ import ResetScrollMixin from '../mixins/reset-scroll';
 
 export default Ember.Route.extend(ResetScrollMixin, {
 
-	isShowingModal: false,
-  actions: {
-    toggleModal: function() {
-      this.toggleProperty('isShowingModal');
-    }
-  },
-	activate: function() {
-		console.log('activate');
-		this.toggleProperty('isShowingModal');
-		setTimeout(function() {
-			$('.ember-modal-dialog').css({'right':'2%'});
-		},2000)
-
-	}
-
 });
